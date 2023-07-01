65b83eb66e75827217df936149229ae466f3c9cd
Zenny Chen <zenny_chen@sina.com>
2023-07-01 21:43:11 +0800

    Add files via upload

--- VulkanGLSL/VulkanGLSL/shaders/basic.comp.glsl
+++ VulkanGLSL/VulkanGLSL/shaders/basic.comp.glsl
@@ -1,11 +1,29 @@
-#version 450
+#version 460
 #extension GL_EXT_shader_explicit_arithmetic_types : enable
+
+/** ======== Preprocessor Test ======== **/
+// null directive
+#
+// preceded by spaces
+    #
+// followed by spaces and precedint the directive
+#    define    MY_VALUE1    1
+// Commonly used
+#define     MY_VALUE6       6
+
+// ## operator is not supported by `glslangValidator` tool.
+#define MY_CONSTANT_CONSTRUCT(a, b)      a ## b
+
+// 'MY_CONSTANT_VALUE' is used at line 26, so here `__LINE__` will substitute 26.
+// The complete 'MY_CONSTANT_VALUE' used at line 26 will substitute 100.
+// filename-based `__FILE__` required extension: `GL_GOOGLE_cpp_style_line_directive`
+#define MY_CONSTANT_VALUE   ((MY_VALUE1 * __LINE__ - MY_VALUE6) * 5 +  __VERSION__ - 460)
 
 layout(local_size_x_id = 0, local_size_y_id = 1, local_size_z_id = 2) in;
 
 // This constant will be specialized when creating the compute pipeline.
 // Its default value is 100.
-layout(constant_id = 3) const int spec_constant = 100;
+layout(constant_id = 3) const int spec_constant = MY_CONSTANT_VALUE;
 
 struct CustomPushConstants
 {
@@ -30,7 +48,7 @@ layout(std430, set = 0, binding = 1) buffer readonly Src {
 };
 
 // Private constant
-const uint g_constant = 2;
+const uint g_constant = 2U;
 
 // Private variable
 uint g_gtid = 0;
